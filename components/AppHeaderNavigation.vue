319b8b081ca4aff9fba635a9165da4c2055ac459
Alexandru <videoloss3@gmail.com>
2023-07-05 01:20:50 +0300

    improve: LDP-2191: Add branding elements and header

---
+++ components/AppHeaderNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="header-nav">
+    <nuxt-link to="/get-started/how-it-works"> How it works </nuxt-link>
+    <nuxt-link to="/guide/pages-routing"> Docs </nuxt-link>
+    <nuxt-link to="/get-started/play-online"> Examples </nuxt-link>
+    <nuxt-link to="/get-started/play-online"> Community </nuxt-link>
+  </nav>
+</template>
+
+<style scoped>
+.header-nav {
+  display: none;
+}
+
+@media (min-width: 1200px) {
+  .header-nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  .header-nav a:hover {
+    color: #fffffe;
+  }
+}
+</style>
